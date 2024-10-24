3be844c65cbf70cf8371bca9bcd241b0ca9511b8
MrDPrasad <durgaprasadn028@gmail.com>
2024-10-24 12:26:33 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import * as XLSX from 'xlsx';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -37,5 +37,17 @@ function login(username, password) {
   } else {
     alert('Login failed');
   }
+}
+function importExcel(file) {
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    const data = new Uint8Array(event.target.result);
+    const workbook = XLSX.read(data, { type: 'array' });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const json = XLSX.utils.sheet_to_json(worksheet);
+    console.log(json);
+  };
+  reader.readAsArrayBuffer(file);
 }
 export default App
